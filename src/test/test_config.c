3a79d23fd24b9809abef7f7e939bff9c6515c796
Fan Jiang <fanjiang@thoughtworks.com>
2015-08-25 15:51:20 -0500

    we can't test RunAsDaemon if it fork new process

--- src/test/test_config.c
+++ src/test/test_config.c
@@ -4026,20 +4026,6 @@ static void test_config_options_act_ServerTransportPlugin_err(void *arg)
     (void)arg;
 }
 
-static void test_config_options_act_RunAsDaemon(void *arg)
-{
-    or_options_t *options, *old_options;
-    old_options = options_new();
-    options = get_options_mutable();
-    options_init(options);
-    options->command = CMD_RUN_TOR;
-    options->RunAsDaemon = 1;
-    tt_int_op(options_act(old_options),OP_EQ,0);
-  done:
-    options->RunAsDaemon = 0;
-    (void)arg;
-}
-
 static void test_config_options_act_options_transition_requires_fresh_tls_context(void *arg)
 {
     or_options_t *options, *old_options;
@@ -4091,7 +4077,6 @@ struct testcase_t config_tests[] = {
   CONFIG_TEST(options_act_Bridge_err, 0),
   CONFIG_TEST(options_act_ClientTransportPlugin_err, 0),
   CONFIG_TEST(options_act_ServerTransportPlugin_err, 0),
-  CONFIG_TEST(options_act_RunAsDaemon, 0),
   CONFIG_TEST(options_act_options_transition_requires_fresh_tls_context, 0),
   END_OF_TESTCASES
 };
