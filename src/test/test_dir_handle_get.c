0d180b0f58efbca6c29ce255836ab12e6a4d854f
Reinaldo de Souza Jr <juniorz@gmail.com>
2015-08-27 15:59:12 -0500

    Add test for server descriptor req with 'd' param

--- src/test/test_dir_handle_get.c
+++ src/test/test_dir_handle_get.c
@@ -768,6 +768,66 @@ test_dir_handle_get_server_descriptors_invalid_req(void* data)
     tor_free(header);
 }
 
+static void
+test_dir_handle_get_server_descriptors_all(void* data)
+{
+  dir_connection_t *conn = NULL;
+  char *header = NULL;
+  char *body = NULL;
+  size_t body_used = 0;
+  (void) data;
+
+  //TODO: change to router_get_my_extrainfo when testing "extra" path
+  NS_MOCK(router_get_my_routerinfo);
+  MOCK(connection_write_to_buf_impl_, connection_write_to_buf_mock);
+
+  /* Setup fake routerlist. */
+  helper_setup_fake_routerlist();
+
+  // We are one of the routers
+  routerlist_t *our_routerlist = router_get_routerlist();
+  tt_int_op(smartlist_len(our_routerlist->routers), OP_GE, 1);
+  mock_routerinfo = smartlist_get(our_routerlist->routers, 0);
+  set_server_identity_key(mock_routerinfo->identity_pkey);
+
+  /* Treat "all" requests as if they were unencrypted */
+  mock_routerinfo->cache_info.send_unencrypted = 1;
+
+  conn = dir_connection_new(tor_addr_family(&MOCK_TOR_ADDR));
+
+  tt_int_op(directory_handle_command_get(conn, SERVER_DESC_GET("all"), NULL, 0), OP_EQ, 0);
+
+  //TODO: Is this a BUG?
+  //It requires strlen(signed_descriptor_len)+1 as body_len but returns a body which
+  //is smaller than that by annotation_len bytes
+  fetch_from_buf_http(TO_CONN(conn)->outbuf, &header, MAX_HEADERS_SIZE,
+                      &body, &body_used, mock_routerinfo->cache_info.signed_descriptor_len+1, 0);
+
+  tt_assert(header);
+  tt_assert(body);
+
+  tt_ptr_op(strstr(header, "HTTP/1.0 200 OK\r\n"), OP_EQ, header);
+  tt_assert(strstr(header, "Content-Type: text/plain\r\n"));
+  tt_assert(strstr(header, "Content-Encoding: identity\r\n"));
+
+  //TODO: Is this a BUG?
+  //This is what should be expected:
+  //tt_int_op(body_used, OP_EQ, strlen(body));
+  tt_int_op(body_used, OP_EQ, mock_routerinfo->cache_info.signed_descriptor_len);
+
+  tt_str_op(body, OP_EQ, mock_routerinfo->cache_info.signed_descriptor_body + mock_routerinfo->cache_info.annotations_len);
+  tt_int_op(conn->dir_spool_src, OP_EQ, DIR_SPOOL_NONE);
+
+  done:
+    NS_UNMOCK(router_get_my_routerinfo);
+    UNMOCK(connection_write_to_buf_impl_);
+    tor_free(conn);
+    tor_free(header);
+
+    routerlist_free_all();
+    nodelist_free_all();
+    entry_guards_free_all();
+}
 
 static char
 TEST_DESCRIPTOR[] =
@@ -805,73 +865,6 @@ TEST_DESCRIPTOR[] =
 "eChGPgFNUvEc+5YtD27qEDcUjcinYztTs7/dzxBT4PE=\n"
 "-----END SIGNATURE-----\n";
 
-static void
-test_dir_handle_get_server_descriptors_all(void* data)
-{
-  dir_connection_t *conn = NULL;
-  char *header = NULL;
-  char *body = NULL;
-  size_t body_used = 0;
-  (void) data;
-
-  //TODO: change to router_get_my_extrainfo when testing "extra" path
-  NS_MOCK(router_get_my_routerinfo);
-  MOCK(connection_write_to_buf_impl_, connection_write_to_buf_mock);
-
-  /* Setup fake routerlist. */
-  helper_setup_fake_routerlist();
-
-  // We are one of the routers
-  routerlist_t *our_routerlist = router_get_routerlist();
-  routerinfo_t *router = smartlist_get(our_routerlist->routers, 0);
-  set_server_identity_key(router->identity_pkey);
-
-  /* Treat "all" requests as if they were unencrypted */
-  router_get_my_routerinfo(); //init the mock
-  mock_routerinfo->cache_info.send_unencrypted = 1;
-
-  /* Setup descriptor */
-  int annotation_len = strstr(TEST_DESCRIPTOR, "router ") - TEST_DESCRIPTOR;
-  mock_routerinfo->cache_info.signed_descriptor_body = TEST_DESCRIPTOR;
-  mock_routerinfo->cache_info.signed_descriptor_len = strlen(TEST_DESCRIPTOR);
-  mock_routerinfo->cache_info.annotations_len = annotation_len;
-
-  conn = dir_connection_new(tor_addr_family(&MOCK_TOR_ADDR));
-
-  tt_int_op(directory_handle_command_get(conn, SERVER_DESC_GET("all"), NULL, 0), OP_EQ, 0);
-
-  //TODO: Is this a BUG?
-  //It requires strlen(TEST_DESCRIPTOR)+1 as body_len but returns a body which
-  //is smaller than that by annotation_len bytes
-  fetch_from_buf_http(TO_CONN(conn)->outbuf, &header, MAX_HEADERS_SIZE,
-                      &body, &body_used, strlen(TEST_DESCRIPTOR)+1, 0);
-
-  tt_assert(header);
-  tt_assert(body);
-
-  tt_ptr_op(strstr(header, "HTTP/1.0 200 OK\r\n"), OP_EQ, header);
-  tt_assert(strstr(header, "Content-Type: text/plain\r\n"));
-  tt_assert(strstr(header, "Content-Encoding: identity\r\n"));
-
-  //TODO: Is this a BUG?
-  //This is what should be expected:
-  //tt_int_op(body_used, OP_EQ, strlen(body));
-  tt_int_op(body_used, OP_EQ, strlen(TEST_DESCRIPTOR));
-
-  tt_str_op(body, OP_EQ, TEST_DESCRIPTOR + annotation_len);
-  tt_int_op(conn->dir_spool_src, OP_EQ, DIR_SPOOL_NONE);
-
-  done:
-    NS_UNMOCK(router_get_my_routerinfo);
-    UNMOCK(connection_write_to_buf_impl_);
-    tor_free(mock_routerinfo);
-    tor_free(conn);
-    tor_free(header);
-
-    routerlist_free_all();
-    nodelist_free_all();
-    entry_guards_free_all();
-}
 
 static void
 test_dir_handle_get_server_descriptors_authority(void* data)
@@ -998,6 +991,66 @@ test_dir_handle_get_server_descriptors_fp(void* data)
     crypto_pk_free(identity_pkey);
 }
 
+static void
+test_dir_handle_get_server_descriptors_d(void* data)
+{
+  dir_connection_t *conn = NULL;
+  char *header = NULL;
+  char *body = NULL;
+  size_t body_used = 0;
+  crypto_pk_t *identity_pkey = pk_generate(0);
+  (void) data;
+
+  NS_MOCK(router_get_my_routerinfo);
+  MOCK(connection_write_to_buf_impl_, connection_write_to_buf_mock);
+
+  /* Setup fake routerlist. */
+  helper_setup_fake_routerlist();
+
+  /* Get one router's signed_descriptor_digest */
+  routerlist_t *our_routerlist = router_get_routerlist();
+  tt_int_op(smartlist_len(our_routerlist->routers), OP_GE, 1);
+  routerinfo_t *router = smartlist_get(our_routerlist->routers, 0);
+  const char *hex_digest = hex_str(router->cache_info.signed_descriptor_digest, DIGEST_LEN);
+
+  conn = dir_connection_new(tor_addr_family(&MOCK_TOR_ADDR));
+
+  #define HEX1 "Fe0daff89127389bc67558691231234551193EEE"
+  #define HEX2 "Deadbeef99999991111119999911111111f00ba4"
+  char req_header[155];
+  sprintf(req_header, SERVER_DESC_GET("d/%s+" HEX1 "+" HEX2), hex_digest);
+  tt_int_op(directory_handle_command_get(conn, req_header, NULL, 0), OP_EQ, 0);
+
+  //TODO: Is this a BUG?
+  //It requires strlen(signed_descriptor_len)+1 as body_len but returns a body which
+  //is smaller than that by annotation_len bytes
+  fetch_from_buf_http(TO_CONN(conn)->outbuf, &header, MAX_HEADERS_SIZE,
+                      &body, &body_used, router->cache_info.signed_descriptor_len+1, 0);
+
+  tt_assert(header);
+  tt_assert(body);
+
+  tt_ptr_op(strstr(header, "HTTP/1.0 200 OK\r\n"), OP_EQ, header);
+  tt_assert(strstr(header, "Content-Type: text/plain\r\n"));
+  tt_assert(strstr(header, "Content-Encoding: identity\r\n"));
+
+  //TODO: Is this a BUG?
+  //This is what should be expected:
+  //tt_int_op(body_used, OP_EQ, strlen(body));
+  tt_int_op(body_used, OP_EQ, router->cache_info.signed_descriptor_len);
+
+  tt_str_op(body, OP_EQ, router->cache_info.signed_descriptor_body + router->cache_info.annotations_len);
+  tt_int_op(conn->dir_spool_src, OP_EQ, DIR_SPOOL_NONE);
+
+  done:
+    NS_UNMOCK(router_get_my_routerinfo);
+    UNMOCK(connection_write_to_buf_impl_);
+    tor_free(mock_routerinfo);
+    tor_free(conn);
+    tor_free(header);
+    crypto_pk_free(identity_pkey);
+}
+
 #define DIR_HANDLE_CMD(name,flags)                              \
   { #name, test_dir_handle_get_##name, (flags), NULL, NULL }
 
@@ -1023,5 +1076,6 @@ struct testcase_t dir_handle_get_tests[] = {
   DIR_HANDLE_CMD(server_descriptors_all, TT_FORK),
   DIR_HANDLE_CMD(server_descriptors_authority, TT_FORK),
   DIR_HANDLE_CMD(server_descriptors_fp, TT_FORK),
+  DIR_HANDLE_CMD(server_descriptors_d, TT_FORK),
   END_OF_TESTCASES
 };
