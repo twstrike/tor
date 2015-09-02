54e191b73eff09fb12e551b39d73a91f78ef7ea4
Iván Pazmiño <iapazmino@gmail.com>
2015-09-02 17:05:30 -0500

    Verify connection is closed if hostname is an exit ans that's not allowed

--- src/test/test_conn_edge_ap_handshake.c
+++ src/test/test_conn_edge_ap_handshake.c
@@ -44,16 +44,21 @@ static struct testcase_setup_t test_rewrite_setup = {
 static rewrite_result_t *rewrite_mock = NULL; 
 
 static void
-connection_ap_handshake_rewrite_mock(entry_connection_t *conn, rewrite_result_t *result)
+connection_ap_handshake_rewrite_mock(entry_connection_t *conn,
+                                       rewrite_result_t *result)
 {
   tor_assert(result);
   result->should_close = rewrite_mock->should_close;
   result->end_reason = rewrite_mock->end_reason;
+  result->exit_source = rewrite_mock->exit_source;
   (void) conn;
 }
 
 static void
-connection_mark_unattached_ap_mock(entry_connection_t *conn, int reason, int line, const char *file)
+connection_mark_unattached_ap_mock(entry_connection_t *conn,
+                                     int reason,
+                                     int line,
+                                     const char *file)
 {
   tor_assert(reason);
   (void) conn;
@@ -139,6 +144,35 @@ test_conn_edge_ap_handshake_rewrite_and_attach_closes_conn_when_hostname_is_bogu
     tor_free(path);
 }
 
+static void
+test_conn_edge_ap_handshake_rewrite_and_attach_closes_conn_when_hostname_is_unallowed_exit(void *data)
+{
+  entry_connection_t *conn = data;
+  origin_circuit_t *circuit = NULL;
+  crypt_path_t *path = NULL;
+
+  MOCK(connection_mark_unattached_ap_, connection_mark_unattached_ap_mock);
+  MOCK(connection_ap_handshake_rewrite, connection_ap_handshake_rewrite_mock);
+  rewrite_mock = tor_malloc_zero(sizeof(rewrite_result_t));
+  rewrite_mock->should_close = 0;
+  rewrite_mock->exit_source = ADDRMAPSRC_AUTOMAP;
+  strlcpy(conn->socks_request->address,
+            "http://www.notgood.exit",
+            sizeof(conn->socks_request->address));
+  conn->socks_request->command = SOCKS_COMMAND_CONNECT;
+
+  int res = connection_ap_handshake_rewrite_and_attach(conn, circuit, path);
+
+  tt_int_op(res, OP_EQ, -1);
+
+  done:
+    UNMOCK(connection_ap_handshake_rewrite);
+    UNMOCK(connection_mark_unattached_ap_);
+    tor_free(rewrite_mock);
+    tor_free(circuit);
+    tor_free(path);
+}
+
 #define CONN_EDGE_AP_HANDSHAKE(name,flags)                              \
   { #name, test_conn_edge_ap_handshake_##name, (flags), &test_rewrite_setup, NULL }
 
@@ -147,5 +181,6 @@ struct testcase_t conn_edge_ap_handshake_tests[] =
   CONN_EDGE_AP_HANDSHAKE(rewrite_and_attach_closes_conn_with_answer, 0),
   CONN_EDGE_AP_HANDSHAKE(rewrite_and_attach_closes_conn_with_error, 0),
   CONN_EDGE_AP_HANDSHAKE(rewrite_and_attach_closes_conn_when_hostname_is_bogus, 0),
+  CONN_EDGE_AP_HANDSHAKE(rewrite_and_attach_closes_conn_when_hostname_is_unallowed_exit, 0),
   END_OF_TESTCASES
 };
